87e2c74c56fa2748069cd7587fa8b5c41af8c645
DESKTOP-UOM8S6S\chicong <cong.hoangchi98@gmail.com>
2023-06-15 09:58:53 +0700

    50% excercise - toppic 30 - wait

--- src/webdriver/Topic_28_Wait_Static.java
+++ src/webdriver/Topic_28_Wait_Static.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -14,6 +16,7 @@ public class Topic_28_Wait_Static {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
+	WebDriverWait explicitWait;
 
 	@BeforeClass
 	public void beforeClass() {
@@ -25,14 +28,31 @@ public class Topic_28_Wait_Static {
 
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 10);
 	}
 
-	@Test
-	public void TC_01_Implicit_No_Set() {
+//	@Test
+	public void TC_04_Invisible() {
 		driver.get("https://automationfc.github.io/dynamic-loading/");
 
 		// Click to Start button
 		driver.findElement(By.xpath("//div[@id='start']/button")).click();
+
+		explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@id='loading']")));
+
+		// Verify
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@id='finish']/h4")).getText(), "Hello World!");
+	}
+
+	@Test
+	public void TC_05_Visible() {
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+
+		// Click to Start button
+		driver.findElement(By.xpath("//div[@id='start']/button")).click();
+
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='finish']/h4")));
 
 		// Get text
 		String helloText = driver.findElement(By.xpath("//div[@id='finish']/h4")).getText();
@@ -42,53 +62,11 @@ public class Topic_28_Wait_Static {
 	}
 
 	@Test
-	public void TC_02_Implicit_Not_Enough() {
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-
-		driver.get("https://automationfc.github.io/dynamic-loading/");
-
-		// Click to Start button
-		driver.findElement(By.xpath("//div[@id='start']/button")).click();
-
-		// Get text
-		String helloText = driver.findElement(By.xpath("//div[@id='finish']/h4")).getText();
-
-		// Verify
-		Assert.assertEquals(helloText, "Hello World!");
-	}
-
-	@Test
-	public void TC_03_Implicit_Equal() {
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-		driver.get("https://automationfc.github.io/dynamic-loading/");
-
-		// Click to Start button
-		driver.findElement(By.xpath("//div[@id='start']/button")).click();
-
-		// Get text
-		String helloText = driver.findElement(By.xpath("//div[@id='finish']/h4")).getText();
-
-		// Verify
-		Assert.assertEquals(helloText, "Hello World!");
+	public void TC_06_Telerik() {
+		driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+		
 	}
 	
-	@Test
-	public void TC_03_Implicit_Greater() {
-		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-
-		driver.get("https://automationfc.github.io/dynamic-loading/");
-
-		// Click to Start button
-		driver.findElement(By.xpath("//div[@id='start']/button")).click();
-
-		// Get text
-		String helloText = driver.findElement(By.xpath("//div[@id='finish']/h4")).getText();
-
-		// Verify
-		Assert.assertEquals(helloText, "Hello World!");
-	}
-
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
