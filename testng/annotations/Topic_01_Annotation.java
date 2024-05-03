1b5e55f1dfd3915594b4b9489ab352f3f684e68d
Chi Cong <cong.hoangchi98@gmail.com>
2024-05-03 09:58:04 +0700

    Topic 46 - TestNG - Annotations

---
+++ testng/annotations/Topic_01_Annotation.java
@@ -0,0 +1,25 @@
+package annotations;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Annotation {
+	@Test
+	public void f() {
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void TC_01_Register() {
+
+	}
+
+	@Test
+	public void TC_02_Login() {
+
+	}
+
+}
